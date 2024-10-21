887c4624ba0e9c7e1be2e9915117f9b3b3a44d22
Anonymous <cpim526@gmail.com>
2024-10-21 16:50:42 +0000

    add

--- st.py
+++ st.py
@@ -12,7 +12,10 @@ while True:
     banner.banner()
     control.run_php_server(PORT)
     try:
-        input(" "+Fore.WHITE+Back.RED+"If You Want Exit And Turn Off localhost / press enter or CTRL+C "+Style.RESET_ALL)
+        ngrok_domain = "https://epic-koi-sensibly.ngrok-free.app"
+        input(f" {Fore.WHITE+Back.RED}Access the app at {ngrok_domain}, press Enter or CTRL+C to exit {Style.RESET_ALL}")
+
+        # input(" "+Fore.WHITE+Back.RED+"If You Want Exit And Turn Off localhost / press enter or CTRL+C "+Style.RESET_ALL)
         control.kill_php_proc()
         exit()
     
